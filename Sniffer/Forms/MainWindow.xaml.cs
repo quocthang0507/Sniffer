994644b68c3bc73279eafa36678703b1a41bca68
La Quốc Thắng <quocthang0507@gmail.com>
2020-04-26 17:16:12 +0700

    Add new form

--- Sniffer/Forms/MainWindow.xaml.cs
+++ Sniffer/Forms/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sniffer.Forms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,7 +28,8 @@ namespace Sniffer
 
 		private void btnExit_Click(object sender, RoutedEventArgs e)
 		{
-			Environment.Exit(1);
+			QuitWindow quitWindow = new QuitWindow();
+			quitWindow.ShowDialog();
 		}
 
 		private void btnAbout_Click(object sender, RoutedEventArgs e)
---
+++ Sniffer/Forms/QuitWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Sniffer.Forms
+{
+	/// <summary>
+	/// Interaction logic for QuitWindow.xaml
+	/// </summary>
+	public partial class QuitWindow : Window
+	{
+		public QuitWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void btnSave_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btnExitWithoutSave_Click(object sender, RoutedEventArgs e)
+		{
+			Environment.Exit(1);
+		}
+
+		private void btnCancel_Click(object sender, RoutedEventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
