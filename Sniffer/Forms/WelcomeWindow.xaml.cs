bca4fe433e5d749a9a86f6c803ad656674778b3c
Nguyen Thi Linh <62165724+LinhTinh1005@users.noreply.github.com>
2020-05-11 08:15:54 +0700

    Update by Linh

--- Sniffer/Forms/WelcomeWindow.xaml.cs
+++ Sniffer/Forms/WelcomeWindow.xaml.cs
@@ -1,15 +1,21 @@
-﻿using System.Windows;
+﻿using SnifferLib;
+using System.Windows;
 
 namespace Sniffer.Forms
 {
-	/// <summary>
-	/// Interaction logic for WelcomeWindow.xaml
-	/// </summary>
-	public partial class WelcomeWindow : Window
-	{
-		public WelcomeWindow()
-		{
-			InitializeComponent();
-		}
-	}
+    /// <summary>
+    /// Interaction logic for WelcomeWindow.xaml
+    /// </summary>
+    public partial class WelcomeWindow : Window
+    {
+        public WelcomeWindow()
+        {
+            InitializeComponent();
+            ShowInterfaces();
+        }
+        private void ShowInterfaces()
+        {
+            listInterface.ItemsSource = SnifferClass.GetInterfaces();
+        }
+    }
 }
