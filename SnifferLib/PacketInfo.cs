a5cccca76301c63a868d8c1a6d26cf1ba1c1b189
Nguyen Thi Linh <62165724+LinhTinh1005@users.noreply.github.com>
2020-05-24 08:51:30 +0700

    Update

--- SnifferLib/PacketInfo.cs
+++ SnifferLib/PacketInfo.cs
@@ -8,11 +8,29 @@ namespace SnifferLib
 {
 	public class PacketInfo
 	{
+		/// <summary>
+		/// STT
+		/// </summary>
 		public int ID { get; set; }
+		/// <summary>
+		/// Thời gian
+		/// </summary>
 		public float Time { get; set; }
+		/// <summary>
+		/// IP nguồn
+		/// </summary>
 		public string Source { get; set; }
+		/// <summary>
+		/// Ip đích
+		/// </summary>
 		public string Destination { get; set; }
+		/// <summary>
+		/// Giao thức
+		/// </summary>
 		public string Protocol { get; set; }
+		/// <summary>
+		/// Thông tin
+		/// </summary>
 		public string Info { get; set; }
 
 		public PacketInfo(int iD, float time, string source, string destination, string protocol, string info)
